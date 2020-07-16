962968cdece6557d0fcc14a97d306f7201975f47
Faisal Sayed <faisal.sayed502@gmail.com>
2020-07-16 13:03:30 +0530

    commit on 'master'

---
+++ src/Nav.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './Nav.css';
+
+function Nav() {
+  return (
+    <div className='Navbar-Container'>
+
+      <div className='Navbar1'>
+        <nav className='Nav1'>
+          <h1 className='nav1-child'>Welcome</h1>
+        </nav>
+      </div>
+
+      <div className='Navbar2'>
+
+        <nav className='Nav2'>
+            <a href='#home' className='nav2-child'>Home</a>
+            <a href='#about' className='nav2-child'>About</a>
+            <a href='#project' className='nav2-child'>Projects</a>
+            <a href='#contact' className='nav2-child'>Contact</a>
+        </nav>
+
+      </div>
+
+      <div className='test'></div>
+    </div>
+  );
+}
+
+export default Nav;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Nav from './Nav';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Nav />
   </React.StrictMode>,
   document.getElementById('root')
 );
