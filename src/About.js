44d9c85e9fa57f0d4ce4a35a5b28e1eb7852d972
Faisal Sayed <faisal.sayed502@gmail.com>
2020-07-17 20:57:27 +0530

    commit on 'master'

--- src/About.js
+++ src/About.js
@@ -2,12 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './About.css';
 
-
+const MyImage = 'https://doggo.ninja/IBf0BT.png';
 
 const About = () => {
-    return(
-        <div>
-            <p>lorem ipsum dolor sit.</p>
+    return (
+        <div className= 'About'>
+            <div className='about-container'>
+                <h1 className='about-title'>About Me</h1>
+            </div>
+            
+            <div>
+                <img className='myPic' src={MyImage} alt='My Pic' />
+            </div>
+
+            <div className='about-me'>
+                <p>lorem ipsum dolor sit.</p>
+            </div>
         </div>
     )
 }
