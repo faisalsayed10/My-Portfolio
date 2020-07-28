cbe81e45fa068bb5b8c7b1fd07c823cd9e40e712
Faisal Sayed <faisal.sayed502@gmail.com>
2020-07-28 20:40:17 +0530

    commit on master

--- src/index.js
+++ src/index.js
@@ -3,12 +3,10 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter, Route } from 'react-router-dom'
 import MainBody from './MainBody';
 import JourneyMain from './JourneyMain';
-import Nav from './Nav';
-
 
 const Index = () => {
   return (
-      <BrowserRouter history={history} basename={process.env.PUBLIC_URL}>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Route path='/' exact component={MainBody} />
         <Route path='/journey' exact component={JourneyMain} />
       </BrowserRouter>
