65684f96b5923566fb7ee0a74d02f35f9c93db7c
faisalsayed <faisal.sayed502@gmail.com>
2020-08-22 13:00:30 +0530

    master

--- src/About.js
+++ src/About.js
@@ -1,29 +1,22 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './About.css';
 import './AbtCodingJourney';
 import AbtCodingJourney from './AbtCodingJourney';
-
-const MyImage = 'https://doggo.ninja/IBf0BT.png';
+import MyPic from './MyPic';
 
 const About = () => {
     return (
-        <>
-            <div id='about' className='about-container'>
-                <h1 className='about-title'>About Me</h1>
-            
-            
-            <img className='myPic' src={MyImage} alt='My Pic' />
-
+        <div id='about' className='about-container'>
+            <h1 className='about-title'>About Me</h1>
+            <MyPic />
             <div className='about-me'>
-                <p><text className='txt1'>Hi! I'm Faisal Sayed</text><text className='txt2'> also</text><text className='txt3'> known as Fayd.</text><text className='txt4'> I'm 15</text><text className='txt5'> and I love to code and make games.</text><text className='txt6'> I've learnt</text><text className='txt7'> HTML, CSS,</text><text className='txt8'> JSX, python, </text>
-                <text className='txt9'>Javascript and React Js.</text><text className='txt4'> I love Javascript and React</text><text className='txt3'> the most.</text><text className='txt7'> I can build </text><text className='txt1'>sweet looking front-end websites! </text>
-                <text className='txt6'>I am also a Hack Clubber!</text><text className='txt9'> I love to play games,</text><text className='txt2'> make gaming videos</text><text className='txt8'> and have fun with it. </text>
+                <p><span className='txt1'>Hi! I'm Faisal Sayed</span><span className='txt2'> also</span><span className='txt3'> known as Fayd.</span><span className='txt4'> I'm 15</span><span className='txt5'> and I love to code and make games.</span><span className='txt6'> I've learnt</span><span className='txt7'> HTML, CSS,</span><span className='txt8'> JSX, python, </span>
+                    <span className='txt9'>Javascript and React Js.</span><span className='txt4'> I love Javascript and React</span><span className='txt3'> the most.</span><span className='txt7'> I can build </span><span className='txt1'>sweet looking front-end websites! </span>
+                    <span className='txt6'>I am also a Hack Clubber!</span><span className='txt9'> I love to play games,</span><span className='txt2'> make gaming videos</span><span className='txt8'> and have fun with it. </span>
                 </p>
             </div>
             <AbtCodingJourney />
-            </div>
-        </>
+        </div>
     )
 }
 
--- src/Projects.js
+++ src/Projects.js
@@ -1,54 +1,60 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './Project.css';
-import './MinorProjects.js';
-import MinorProjects from './MinorProjects.js';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./Project.css";
+import "./MinorProjects.js";
+import MinorProjects from "./MinorProjects.js";
 
 const Projects = () => {
-    const projectImage1 = 'https://doggo.ninja/RUtKvm.PNG';
-    const projectLink1 = 'https://hacktoberfest-2020-recreated.webflow.io/';
-    const projectImage2 = 'https://doggo.ninja/p5OrIe.png';
-    const projectImage3 = 'https://doggo.ninja/n3Sw87.png';
-    const projectLink3 = 'https://crassh.webflow.io';
-    const sample_image = 'https://bit.ly/2ZLR3Rf';
-    return(
-        <>
-            <div id='project' className='project-container'>
-                <h1 className='projects-title'>My Projects</h1>
-            
+  const projectImage1 = "https://doggo.ninja/RUtKvm.PNG";
+  const projectLink1 = "https://hacktoberfest-2020-recreated.webflow.io/";
+  const projectImage2 = "https://doggo.ninja/p5OrIe.png";
+  const projectImage3 = "https://doggo.ninja/n3Sw87.png";
+  const projectLink3 = "https://crassh.webflow.io";
+  const sample_image = "https://bit.ly/2ZLR3Rf";
+  return (
+    <>
+      <div id="project" className="project-container">
+        <h1 className="projects-title">My Projects</h1>
 
-            <div className='project-cards'>
-                <div className='cards card-1'>
-                     <a className="removeClass" href={projectLink1} target='_blank'>
-                        <img className='prj-img-1 prj-img' src={projectImage1} alt='Hactoberfest recreated'></img>
-                        <h1 className='card1-title'>Hacktoberfest 2020 Recreated.</h1>
-                        <h1 className='card1-desc'>I recreated the Hactoberfest 2020 website in Webflow!</h1>
-                    </a>
-                </div>
-                <div className='cards card-2'>
-                    <img className='prj-img-2 prj-img' src={projectImage2}></img>
-                    <h1 className='card2-title'>Rius</h1>
-                    <h1 className='card2-desc'>This is my first ever built indie game for Android.
-                     This is a platformer game which revolves around the journey of Rius.</h1>
-                </div>
-                <div className='cards card-3'>
-                    <a className="removeClass" href={projectLink3} target='_blank'>
-                        <img className='prj-img-3 prj-img' src={projectImage3}></img>
-                        <h1 className='card2-title'>Crassh</h1>
-                        <h1 className='card2-desc'>A Website Made With Webflow</h1>
-                        <h1 className='card2-desc'>Following <a className='removeClass' target='_blank' href='https://designcode.io/webflow-course'>This</a> Course</h1>
-                    </a>
-                </div>
-                </div>
-            
-            
-            <div className='minor-proj'>
-                <h1 className='minor-proj-title'>My Other Minor Projects</h1>
-                <MinorProjects />
-            </div>
-            </div>
-        </>
-    )
-}
+        <div className="project-cards">
+          <div className="cards card-1">
+            <a className="removeClass" href={projectLink1} target="_blank">
+              <img
+                className="prj-img-1 prj-img"
+                src={projectImage1}
+                alt="Hactoberfest recreated"
+              ></img>
+              <h1 className="card1-title">Hacktoberfest 2020 Recreated.</h1>
+              <h1 className="card1-desc">
+                I recreated the Hactoberfest 2020 website in Webflow!
+              </h1>
+            </a>
+          </div>
+          <div className="cards card-2">
+            <img className="prj-img-2 prj-img" src={projectImage2}></img>
+            <h1 className="card2-title">Rius</h1>
+            <h1 className="card2-desc">
+              This is my first ever built indie game for Android. This is a
+              platformer game which revolves around the journey of Rius.
+            </h1>
+          </div>
+          <div className="cards card-3">
+            <a className="removeClass" href={projectLink3} target="_blank">
+              <img className="prj-img-3 prj-img" src={projectImage3}></img>
+              <h1 className="card2-title">Crassh</h1>
+              <h1 className="card2-desc">A Website Made With Webflow</h1>
+              <h1 className="card2-desc">Following Course</h1>
+            </a>
+          </div>
+        </div>
 
-export default Projects
+        <div className="minor-proj">
+          <h1 className="minor-proj-title">My Other Minor Projects</h1>
+          <MinorProjects />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Projects;
