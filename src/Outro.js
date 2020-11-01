0aa934de88f3091f75426ede403c042f74444fd4
faisalsayed10 <faisal.sayed502@gmail.com>
2020-11-01 17:34:51 +0530

    removed webring as it doesn't work

--- src/Outro.js
+++ src/Outro.js
@@ -3,34 +3,9 @@ import "./Outro.css";
 
 const Outro = () => {
   return (
-      <div className="main-outro">
-        <div id="webring-wrapper">
-          <a
-            href="https://webring.hackclub.com/"
-            id="previousBtn"
-            className="webring-anchor"
-            title="Previous"
-          >
-            ‹
-          </a>
-          <a
-            href="https://webring.hackclub.com/"
-            className="webring-logo"
-            title="Hack Club Webring"
-            alt="Hack Club Webring"
-          > </a>
-          <a
-            href="https://webring.hackclub.com/"
-            id="nextBtn"
-            className="webring-anchor"
-            title="Next"
-          >
-            ›
-          </a>
-          <script src="https://webring.hackclub.com/public/embed.min.js"></script>
-        </div>
-        <h2 id="outro-text">Made by Faisal Sayed</h2>
-      </div>
+    <div className="main-outro">
+      <h2 id="outro-text">Made by Faisal Sayed :)</h2>
+    </div>
   );
 };
 
