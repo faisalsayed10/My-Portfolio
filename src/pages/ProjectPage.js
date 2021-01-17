36e7f770956c59b1269b1d2f106794c45e2eb2ea
faisalsayed10 <faisal.sayed502@gmail.com>
2021-01-17 18:47:49 +0530

    adding more projects

--- src/pages/ProjectPage.js
+++ src/pages/ProjectPage.js
@@ -9,24 +9,24 @@ const projectLink2 =
   "https://play.google.com/store/apps/details?id=com.faydstudios.rius";
 const projectImage3 = "https://ninja.dog/i0IbpK.PNG";
 const projectLink3 = "https://binger.fayd.me/";
-const projectImage4 = "https://ninja.dog/5A6nXu.PNG";
-const projectLink4 = "https://drum.fayd.me/";
 const projectImage5 = "https://ninja.dog/c687Qe.PNG";
-const projectLink5 = "https://calculator.fayd.me/";
+const projectLink5 = "https://another-simple-calculator.vercel.app/";
 const projectImage6 = "http://u.filein.io/r-p6kCopMl.png";
 const projectLink6 = "https://expenseman.fayd.me/";
 const projectImage7 = "http://u.filein.io/AyRQUdIpdX.png";
 const projectLink7 = "https://todo.fayd.me/";
 const projectImage8 = "http://u.filein.io/NCSXHHL-Xj.png";
 const projectLink8 = "https://weatherer.fayd.me";
-// const projectImage9 = "";
-// const projectLink9 = "";
-// const projectImage10 = "";
-// const projectLink10 = "";
-// const projectImage11 = "";
-// const projectLink11 = "";
-// const projectImage12 = "";
-// const projectLink12 = "";
+const projectImage9 = "http://u.filein.io/pwyW25TB2P.png";
+const projectLink9 = "https://worldalytics.now.sh/";
+const projectImage10 = "http://u.filein.io/EbRSO6Cu6u.png";
+const projectLink10 = "https://fast-feed.now.sh/";
+const projectImage11 = "http://u.filein.io/qNS8qkr8eb.png";
+const projectLink11 = "https://gh-searcher.vercel.app/";
+const projectImage12 = "http://u.filein.io/NE7ixltSiG.png";
+const projectLink12 = "https://pokedex-git-main.fayd.vercel.app/";
+const projectImage13 = "http://u.filein.io/HaPXgxpqs7.png";
+const projectLink13 = "https://codepen-clone.fayd.vercel.app/";
 
 function ProjectPage() {
   return (
@@ -56,13 +56,6 @@ function ProjectPage() {
             A Movie-Search App built with ReactJS and TMDB API
           </ProjectCard>
           <ProjectCard
-            projectImage={projectImage4}
-            projectLink={projectLink4}
-            title="Drum Kit"
-          >
-            A drumkit built with Vanilla JS following Wesbos' Course
-          </ProjectCard>
-          <ProjectCard
             projectImage={projectImage5}
             projectLink={projectLink5}
             title="Calculator"
@@ -89,6 +82,41 @@ function ProjectPage() {
             title="Weatherer"
           >
             Realtime Weather App Built with React JS and Weather API
+          </ProjectCard>
+          <ProjectCard
+            projectImage={projectImage9}
+            projectLink={projectLink9}
+            title="Worldalytics"
+          >
+            World Analytics, at your fingertips
+          </ProjectCard>
+          <ProjectCard
+            projectImage={projectImage10}
+            projectLink={projectLink10}
+            title="Fast Feedback"
+          >
+            Built as a part of React 2025
+          </ProjectCard>
+          <ProjectCard
+            projectImage={projectImage11}
+            projectLink={projectLink11}
+            title="GH Searcher"
+          >
+            Get a detailed data of any github user
+          </ProjectCard>
+          <ProjectCard
+            projectImage={projectImage12}
+            projectLink={projectLink12}
+            title="Pokedex"
+          >
+            Pokemon Database built with ReactJS and SWR
+          </ProjectCard>
+          <ProjectCard
+            projectImage={projectImage13}
+            projectLink={projectLink13}
+            title="Codepen Clone"
+          >
+            A clone version of codepen built with ReactJS
           </ProjectCard>
         </div>
       </div>
